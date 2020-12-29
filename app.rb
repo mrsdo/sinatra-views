aacdb7f3cb692f17681a5f3c500cb7f03a7fb201
Martinique Dolce <me@martidolce.com>
2020-12-29 09:41:13 -0500

    spec passed
    Sinatra Views all specs passed

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 require_relative 'config/environment'
-
+# Class APP Router
 class App < Sinatra::Base
-
-	get '/' do
-		
-	end
+  get '/' do
+    erb :index
+  end
+  get '/info' do
+    erb :info
+  end
 end
